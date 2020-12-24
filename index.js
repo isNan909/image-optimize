279a627955f50af63ca1fe9090db2e89aed655d2
Ishan Manandhar <ishan02016@gmail.com>
2020-12-24 17:59:48 +0545

    script written for image optimization

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const sharp = require('sharp');
+
+let directory_name = 'images';
+
+let filenames = fs.readdirSync(directory_name);
+
+filenames.forEach((file) => {
+  const fileFormat = getExtension(file);
+  if (fileFormat === 'svg') {
+    console.log('svg not processed with sharp');
+    return;
+  }
+
+  let sh = sharp('./images/' + file);
+  if (fileFormat === 'jpg') {
+    sh = sh.jpeg({ quality: 70 });
+  } else if (fileFormat === 'png') {
+    sh = sh.png({ quality: 70 });
+  }
+
+  sh.toFile('output/' + file, function (err, info) {
+    console.log(info);
+    if (err) {
+      console.log('error in image optimization');
+      return;
+    }
+  });
+});
+
+function getExtension(filename) {
+  let ext = path.extname(filename || '').split('.');
+  return ext[ext.length - 1];
+}
